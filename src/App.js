96740e674ef74468ee272ce6a343de36ac9550cd
KarolisGaiv <kgaivenis@gmail.com>
2022-05-27 11:31:17 +0300

    Fix font importing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import "./styles/main.scss";
+
 function App() {
   return (
-    <div className="container">
-      <section className="bg-dark"></section>
+    <div>
+      00 Home 01 Destination 02 Crew 03 Technology So, you want to travel to
+      Space Let’s face it; if you want to go to space, you might as well
+      genuinely go to outer space and not hover kind of on the edge of it. Well
+      sit back, and relax because we’ll give you a truly out of this world
+      experience! Explore
     </div>
   );
 }
