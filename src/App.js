1108a845ef99bd503785df52514cabfd65d1b588
KarolisGaiv <kgaivenis@gmail.com>
2022-05-27 11:37:24 +0300

    Add header to homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,37 @@ import "./styles/main.scss";
 function App() {
   return (
     <div>
+      <header className="primary-header">
+        <div></div>
+        <nav>
+          <ul className="primary-navigation underline-indicators flex">
+            <li className="active">
+              <a
+                className="uppercase text-white letter-spacing-2"
+                href="index.html"
+              >
+                <span>01</span>Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="uppercase text-white letter-spacing-2"
+                href="destination.html"
+              >
+                <span>02</span>Destination
+              </a>
+            </li>
+            <li>
+              <a
+                className="uppercase text-white letter-spacing-2"
+                href="technology.html"
+              >
+                <span>03</span>Technology
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
       00 Home 01 Destination 02 Crew 03 Technology So, you want to travel to
       Space Let’s face it; if you want to go to space, you might as well
       genuinely go to outer space and not hover kind of on the edge of it. Well
