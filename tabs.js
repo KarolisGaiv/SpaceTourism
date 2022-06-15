d8f8a4a152f8287c5d5136a29bb17a29b25f9d90
KarolisGaiv <kgaivenis@gmail.com>
2022-06-15 16:14:00 +0300

    Setup planet buttons for keyboard navigation

---
+++ tabs.js
@@ -0,0 +1,13 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', (e) => {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    // change tabindex of current tab to -1
+
+    // if right key is pushed move to next tab on the right
+
+    // if left key is pushed move to next tab on the left
+})
