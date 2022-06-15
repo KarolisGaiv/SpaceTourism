e6ecf0c8f258c2129cd0b3dfaba612539dc0b531
KarolisGaiv <kgaivenis@gmail.com>
2022-06-15 16:23:36 +0300

    Add funcionality to navigatie planet buttons with keyboard arrows

--- tabs.js
+++ tabs.js
@@ -1,13 +1,25 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+let tabFocus = 0;
+
 tabList.addEventListener('keydown', (e) => {
     const keydownLeft = 37;
     const keydownRight = 39;
 
     // change tabindex of current tab to -1
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1)
+    }
 
-    // if right key is pushed move to next tab on the right
+    if (e.keyCode === keydownRight) {
+        tabFocus++;
+    }
 
-    // if left key is pushed move to next tab on the left
+    if (e.keyCode === keydownLeft) {
+        tabFocus--;
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
 })
